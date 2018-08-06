22b1839fe9c5e6818f207ce3038268fd40d2495a
Unknown <40722602+ahakola@users.noreply.github.com>
2018-08-06 17:57:07 +0300

    Rewrote the addon.
    Two new text-lines for translators!
    
    Now instead of a custom UI, addon uses Blizzard's own GuildUI and Communities Frames with custom Tabs.
    
    This version should get rid of the taints, use less memory and doesn't use Global namespace.
    
    There are drawbacks to this change like playing two sounds at once when changing between 'Chat'-tab and any other tab and I had to let go of the 'Always open to Default Tab' feature at least for now. Please let me know if you can help me bring it back.

--- Core.lua
+++ Core.lua
@@ -7,32 +7,10 @@
 	Sanex @ EU-Arathor / ahak @ Curseforge
 
 	/run GuildFrame_Toggle()
-	/run ClassicGuildFrame_Toggle()
+	/run ToggleCommunitiesFrame()
+	/run ToggleGuildFinder()
 ----------------------------------------------------------------------------]]--
 local ADDON_NAME, _ = ...
-
-local originalShowUIPanel = ShowUIPanel
-function ShowUIPanel(frame, force)
-	--print("Hook:", frame:GetName()) -- Debug
-	if frame:GetName() == "CommunitiesFrame" then -- Replace CommunitiesFrame with ClassicGuildUI
-		return ClassicGuildFrame_Toggle()
-	else -- Let rest go through as usual
-		return originalShowUIPanel(frame, force)
-	end
-end
-
-local function initDB(db, defaults) -- This function copies values from one table into another:
-	if type(db) ~= "table" then db = {} end
-	if type(defaults) ~= "table" then return db end
-	for k, v in pairs(defaults) do
-		if type(v) == "table" then
-			db[k] = initDB(db[k], v)
-		elseif type(v) ~= type(db[k]) then
-			db[k] = v
-		end
-	end
-	return db
-end
 
 local L = {}
 do -- Translations
@@ -42,8 +20,10 @@ do -- Translations
 	L.cWarningText = "You have to leave at least one tab enabled!"
 	L.defaultTab = "Default Tab for the first opening:"
 	L.dWarningText = "You have to select tab that is enabled!"
-	L.alwaysDefault = "Always open to Default Tab:"
-	L.alwaysDefaultHelp = "If disabled, addon will open to the last open tab after first opening."
+	L.miniModeDefault = "Open Chat in Minimized-mode"
+	L.miniModeDefaultHelp = "Pressing the Maximize/Minimize-button on the Chat-frame will change this."
+	--L.alwaysDefault = "Always open to Default Tab:"
+	--L.alwaysDefaultHelp = "If disabled, addon will open to the last open tab after first opening."
 
 	if LOCALE == "deDE" then
 
@@ -74,7 +54,14 @@ do -- Translations
 	end
 end
 
-local tabNames = {
+local communitiesTabs = { -- Parent-keys of CommunitiesUI sidetabs
+	"ChatTab",
+	"RosterTab",
+	"GuildBenefitsTab",
+	"GuildInfoTab"
+}
+
+local tabNames = { -- Names of the tabs
 	COMMUNITIES_CHAT_TAB_TOOLTIP,
 	GUILD_TAB_NEWS,
 	GUILD_TAB_ROSTER,
@@ -92,23 +79,217 @@ local defaults = { -- This table defines the addon's default settings:
 		true, -- Rewards
 		true -- Info
 	},
+	miniMode = false,
 	defaultTab = 1,
 	openAlwaysToDefault = false
 }
 
+local cfg
 local f = CreateFrame("Frame")
 f:SetScript("OnEvent", function(self, event, ...)
 	return self[event] and self[event](self, event, ...)
 end)
 f:RegisterEvent("ADDON_LOADED")
 
+local classicTabFrame = CreateFrame("Frame", ADDON_NAME.."Tabs", UIParent)
+classicTabFrame:SetSize(10, 10)
+
+local function initDB(db, defaults) -- This function copies values from one table into another:
+	if type(db) ~= "table" then db = {} end
+	if type(defaults) ~= "table" then return db end
+	for k, v in pairs(defaults) do
+		if type(v) == "table" then
+			db[k] = initDB(db[k], v)
+		elseif type(v) ~= type(db[k]) then
+			db[k] = v
+		end
+	end
+	return db
+end
+
+local function _TabShow(self, ...) -- Resize tabs on show
+	PanelTemplates_TabResize(self, 0)
+end
+
+local function _TabClick(self, ...) -- Handle Tab clicks
+	local tabIndex = self:GetID();
+	PanelTemplates_SetTab(classicTabFrame, tabIndex)
+	PlaySound(SOUNDKIT.IG_CHARACTER_INFO_TAB)
+
+	if tabIndex == 1 then -- Chat
+		HideUIPanel(_G.GuildFrame)
+		HideUIPanel(_G.LookingForGuildFrame)
+		ShowUIPanel(_G.CommunitiesFrame)
+	elseif tabIndex > 1 then -- GuildUI
+		if IsInGuild() then
+			GuildFrame_LoadUI()
+			if GuildFrame_Toggle then
+				ShowUIPanel(_G.GuildFrame)
+			end
+		else
+			LookingForGuildFrame_LoadUI()
+			if LookingForGuildFrame_Toggle then
+				ShowUIPanel(_G.LookingForGuildFrame)
+			end
+		end
+		HideUIPanel(_G.CommunitiesFrame)
+
+		if ( tabIndex == 2 ) then -- News
+			GuildFrame_TabClicked(_G["GuildFrameTab1"])
+		elseif ( tabIndex == 3 ) then -- Roster
+			GuildFrame_TabClicked(_G["GuildFrameTab2"])
+		elseif ( tabIndex == 4 ) then -- Perks
+			GuildFrame_TabClicked(_G["GuildFrameTab3"])
+		elseif ( tabIndex == 5 ) then -- Rewards
+			GuildFrame_TabClicked(_G["GuildFrameTab4"])
+		elseif ( tabIndex == 6 ) then -- Info
+			GuildFrame_TabClicked(_G["GuildFrameTab5"])
+		end
+	end
+end
+
+local function _createClassicTabs() -- Create new tabs for Classic Guild Frame
+	classicTabFrame.Tabs = classicTabFrame.Tabs or {}
+	for i = 1, 6 do
+		local tab = CreateFrame("Button", "ClassicTab"..i, classicTabFrame, "CharacterFrameTabButtonTemplate", i)
+		if i == 1 then
+			tab:SetPoint("BOTTOMLEFT", 0, -20)
+		else
+			tab:SetPoint("LEFT", classicTabFrame.Tabs[i-1], "RIGHT", -15, 0)
+		end
+		tab:SetText(tabNames[i])
+		tab:SetScript("OnShow", _TabShow)
+		tab:SetScript("OnClick", _TabClick)
+
+		classicTabFrame.Tabs[i] = tab
+	end
+	PanelTemplates_SetNumTabs(classicTabFrame, 6)
+	classicTabFrame:Hide()
+end
+
+local function _HandleTabs() -- Handle hiding and anchoring Classic Guild Frame tabs
+	local firstTab = false
+	local previousTab = 0
+	for i, show in ipairs(cfg.show) do
+		if show then
+			classicTabFrame.Tabs[i]:Show()
+			classicTabFrame.Tabs[i]:ClearAllPoints()
+			if not firstTab then
+				firstTab = true
+				classicTabFrame.Tabs[i]:SetPoint("BOTTOMLEFT", 0, -20)
+			else
+				classicTabFrame.Tabs[i]:SetPoint("LEFT", classicTabFrame.Tabs[previousTab], "RIGHT", -15, 0)
+			end
+			previousTab = i
+		else
+			classicTabFrame.Tabs[i]:Hide()
+		end
+	end
+
+	if not PanelTemplates_GetSelectedTab(classicTabFrame) then
+		_TabClick(classicTabFrame.Tabs[cfg.defaultTab])
+	end
+end
+
+local function _hideBlizzardTabs(self) -- Hide Blizzard's own tabs
+	if self:GetName() == "CommunitiesFrame" then -- Blizzard_Communities
+		for _, key in ipairs(communitiesTabs) do
+			_G.CommunitiesFrame[key]:Hide()
+		end
+	else -- Blizzard_GuildUI
+		for i = 1, 5 do
+			_G["GuildFrameTab"..i]:Hide()
+		end
+	end
+	if not _G["ClassicTab1"] then
+		_createClassicTabs()
+	end
+	classicTabFrame:SetParent(self)
+	classicTabFrame:SetPoint("TOPLEFT", self, "BOTTOMLEFT")
+	classicTabFrame:Show()
+	_HandleTabs()
+end
+
+local function _MinimizeHook()
+	cfg.miniMode = true
+end
+
+local function _MaximizeHook()
+	cfg.miniMode = false
+end
+
 function f:ADDON_LOADED(event, addon)
-	if addon ~= ADDON_NAME then return end
-	self:UnregisterEvent(event)
+	if addon == ADDON_NAME then
+		ClassicGuildFrameConfig = initDB(ClassicGuildFrameConfig, defaults)
+		cfg = ClassicGuildFrameConfig
 
-	ClassicGuildFrameConfig = initDB(ClassicGuildFrameConfig, defaults)
+		if IsAddOnLoaded("Blizzard_Communities") and _G.CommunitiesFrame then
+			_G.CommunitiesFrame:HookScript("OnShow", _hideBlizzardTabs)
+			hooksecurefunc(_G.CommunitiesFrame.MaximizeMinimizeFrame, "Minimize", _MinimizeHook)
+			hooksecurefunc(_G.CommunitiesFrame.MaximizeMinimizeFrame, "Maximize", _MaximizeHook)
+		end
+		if IsAddOnLoaded("Blizzard_GuildUI") and _G.GuildFrame then
+			_G.GuildFrame:HookScript("OnShow", _hideBlizzardTabs)
+		end
+	elseif addon == "Blizzard_Communities" then
+		if IsAddOnLoaded(ADDON_NAME) then
+			_G.CommunitiesFrame:HookScript("OnShow", _hideBlizzardTabs)
+			hooksecurefunc(_G.CommunitiesFrame.MaximizeMinimizeFrame, "Minimize", _MinimizeHook)
+			hooksecurefunc(_G.CommunitiesFrame.MaximizeMinimizeFrame, "Maximize", _MaximizeHook)
+		end
+		if IsAddOnLoaded("Blizzard_GuildUI") and _G.GuildFrame then
+			_G.GuildFrame:HookScript("OnShow", _hideBlizzardTabs)
+		end
+	elseif addon == "Blizzard_GuildUI" then
+		if IsAddOnLoaded(ADDON_NAME) then
+			_G.GuildFrame:HookScript("OnShow", _hideBlizzardTabs)
+		end
+		if IsAddOnLoaded("Blizzard_Communities") and _G.CommunitiesFrame then
+			_G.CommunitiesFrame:HookScript("OnShow", _hideBlizzardTabs)
+			hooksecurefunc(_G.CommunitiesFrame.MaximizeMinimizeFrame, "Minimize", _MinimizeHook)
+			hooksecurefunc(_G.CommunitiesFrame.MaximizeMinimizeFrame, "Maximize", _MaximizeHook)
+		end
+	else return end
+end
 
-	self.ADDON_LOADED = nil
+local originalShowUIPanel = ShowUIPanel
+function ShowUIPanel(frame, force)
+	local function _checkChatFrameSize() -- Set Chat-frame Maximized/Minimized based on setting
+		if cfg.miniMode then
+			_G.CommunitiesFrame.MaximizeMinimizeFrame:Minimize()
+		else
+			_G.CommunitiesFrame.MaximizeMinimizeFrame:Maximize()
+		end
+	end
+
+	--print("ShowUIPanel Hook:", frame:GetName()) -- Debug
+	if frame:GetName() == "CommunitiesFrame" then -- Replace CommunitiesFrame with ClassicGuildUI
+		if not _G.GuildFrame then
+			GuildFrame_LoadUI()
+		end
+		if _G.GuildFrame:IsShown() then -- GuildUI is open, close it to mimic Toggle-action
+			HideUIPanel(_G.GuildFrame)
+			return
+		else
+			if not PanelTemplates_GetSelectedTab(classicTabFrame) then
+				if cfg.defaultTab == 1 then
+					_checkChatFrameSize()
+					return originalShowUIPanel(frame, force)
+				else
+					return originalShowUIPanel(_G.GuildFrame, force)
+				end
+			else
+				if PanelTemplates_GetSelectedTab(classicTabFrame) == 1 then
+					_checkChatFrameSize()
+					return originalShowUIPanel(frame, force)
+				else
+					return originalShowUIPanel(_G.GuildFrame, force)
+				end
+			end
+		end
+	else -- Let rest go through as usual
+		return originalShowUIPanel(frame, force)
+	end
 end
 
 do -- Blizzard Options
@@ -117,10 +298,9 @@ do -- Blizzard Options
 	InterfaceOptions_AddCategory(Options)
 
 	Options:Hide()
-
 	Options:SetScript("OnShow", function(self)
 		local cfg = ClassicGuildFrameConfig
-		local Title, EnableTabsText, CWarningText, DefaultDropDownText, DWarningText, DefaultDropDown, AlwaysDefaultText, AlwaysDefaultCheckBox, AlwaysDefaultHelpText
+		local Title, EnableTabsText, CWarningText, DefaultDropDownText, DWarningText, DefaultDropDown, MiniModeDefaultText, MiniModeDefaultCheckBox, MiniModeDefaultHelpText
 		local function CheckBoxOnClick(button)
 			local checked = not not button:GetChecked()
 
@@ -146,16 +326,22 @@ do -- Blizzard Options
 					if show then
 						cfg.defaultTab = i
 						L_UIDropDownMenu_SetSelectedValue(_G[ADDON_NAME.."OptionsDefaultDropDown"], cfg.defaultTab)
+						_TabClick(classicTabFrame.Tabs[i])
 						break
 					end
 				end
 			end
+			if not cfg.show[PanelTemplates_GetSelectedTab(classicTabFrame)] then
+				_TabClick(classicTabFrame.Tabs[cfg.defaultTab])
+			end
+			_HandleTabs()
 		end
 		local function DropDownOnClick(button)
 			if cfg.show[button.value] then
 				cfg.defaultTab = button.value
 				L_UIDropDownMenu_SetSelectedValue(_G[ADDON_NAME.."OptionsDefaultDropDown"], cfg.defaultTab)
 				DWarningText:Hide()
+				_TabClick(classicTabFrame.Tabs[button.value])
 			else
 				UIFrameFadeOut(DWarningText, 5, 1, 0)
 			end
@@ -193,7 +379,7 @@ do -- Blizzard Options
 			checkbox:SetChecked(cfg.show[i])
 		end
 
-		CWarningText = self:CreateFontString("$parentCWarningText", "ARTWORK", "GameFontNormalSmall") --"GameFontHighlightSmall")
+		CWarningText = self:CreateFontString("$parentCWarningText", "ARTWORK", "GameFontNormalSmall")
 		CWarningText:SetPoint("TOPLEFT", "$parentCheckButton"..#tabNames, "BOTTOMLEFT", 0, -8)
 		CWarningText:SetJustifyH("LEFT")
 		CWarningText:SetText(L.cWarningText)
@@ -210,33 +396,37 @@ do -- Blizzard Options
 		L_UIDropDownMenu_JustifyText(DefaultDropDown, "CENTER")
 		DefaultDropDown:SetPoint("TOPLEFT", DefaultDropDownText, "BOTTOMLEFT", 10, -12)
 
-		DWarningText = self:CreateFontString("$parentCWarningText", "ARTWORK", "GameFontNormalSmall") --"GameFontHighlightSmall")
+		DWarningText = self:CreateFontString("$parentCWarningText", "ARTWORK", "GameFontNormalSmall")
 		DWarningText:SetPoint("TOPLEFT", DefaultDropDown, "BOTTOMLEFT", 0, -8)
 		DWarningText:SetJustifyH("LEFT")
 		DWarningText:SetText(L.dWarningText)
 		DWarningText:Hide()
 
-		AlwaysDefaultText = self:CreateFontString("$parentAlwaysDefault", "ARTWORK", "GameFontHighlight")
-		AlwaysDefaultText:SetPoint("TOPLEFT", DWarningText, "BOTTOMLEFT", -10, -8)
-		AlwaysDefaultText:SetJustifyH("LEFT")
-		AlwaysDefaultText:SetText(L.alwaysDefault)
 
-		AlwaysDefaultCheckBox = CreateFrame("CheckButton", "$parentAlwaysDefaultCheckButton", self, "InterfaceOptionsCheckButtonTemplate")
-		AlwaysDefaultCheckBox:SetPoint("TOPLEFT", AlwaysDefaultText, "BOTTOMLEFT", 10, -8)
-		AlwaysDefaultCheckBox.Text:SetText(ENABLE)
-		AlwaysDefaultCheckBox:SetScript("OnClick", function(button)
+		MiniModeDefaultText = self:CreateFontString("$parentMiniModeDefault", "ARTWORK", "GameFontHighlight")
+		MiniModeDefaultText:SetPoint("TOPLEFT", DWarningText, "BOTTOMLEFT", -10, -8)
+		MiniModeDefaultText:SetJustifyH("LEFT")
+		MiniModeDefaultText:SetText(L.miniModeDefault)
+
+		MiniModeDefaultCheckBox = CreateFrame("CheckButton", "$parentMiniModeDefaultCheckButton", self, "InterfaceOptionsCheckButtonTemplate")
+		MiniModeDefaultCheckBox:SetPoint("TOPLEFT", MiniModeDefaultText, "BOTTOMLEFT", 10, -8)
+		MiniModeDefaultCheckBox.Text:SetText(MINIMIZE)
+		MiniModeDefaultCheckBox:SetScript("OnClick", function(button)
 			local checked = not not button:GetChecked()
 
 			PlaySound(checked and SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON or SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_OFF)
 
-			cfg.openAlwaysToDefault = checked
+			cfg.miniMode = checked
 		end)
-		AlwaysDefaultCheckBox:SetChecked(cfg.openAlwaysToDefault)
+		MiniModeDefaultCheckBox:SetScript("OnShow", function(self)
+			-- This can be changed outside of Options so we need it to be reactive
+			self:SetChecked(cfg.miniMode)
+		end)
 
-		AlwaysDefaultHelpText = self:CreateFontString("$parentAlwaysDefault", "ARTWORK", "GameFontNormalSmall")
-		AlwaysDefaultHelpText:SetPoint("TOPLEFT", AlwaysDefaultCheckBox, "BOTTOMLEFT", 0, -8)
-		AlwaysDefaultHelpText:SetJustifyH("LEFT")
-		AlwaysDefaultHelpText:SetText(L.alwaysDefaultHelp)
+		MiniModeDefaultHelpText = self:CreateFontString("$parentMiniModeDefaultHelp", "ARTWORK", "GameFontNormalSmall")
+		MiniModeDefaultHelpText:SetPoint("TOPLEFT", MiniModeDefaultCheckBox, "BOTTOMLEFT", 0, -8)
+		MiniModeDefaultHelpText:SetJustifyH("LEFT")
+		MiniModeDefaultHelpText:SetText(L.miniModeDefaultHelp)
 
 		self:SetScript("OnShow", nil)
 	end)
--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Restores the look and functionality of the old GuildUI, but adds minimized Chat from the new CommunitiesUI.
 
-## !! TAINT WARNING !!
-#### Why is this addon causing taints?
-Blizzard's own implementation of Lua ingame isn't perfect and using these functions and templates can cause some weird chains of events resulting in taints for no obvious reason. I already replaced all the `UIDropDownMenu`-related calls and templates with library to eliminate those taints, but they weren't the only source of taints.
-#### If you happen to find the remaining source(s) of taints in this addon, I'm more than happy to patch it!
-#### Even with taints this addon works 99,99% of the time; the only thing the taints prevent is `creating`, `editing` and `removing` subchannels in the Guild and Communities Chat-system.
+## ABOUT TAINTS
+Unlike the previous versions with custom UI, this new version uses Blizzard's own `Blizzard_GuildUI` and `Blizzard_Communities` and **should** be taint-free. In my own tests, everything worked and I couldn't find any taints, but if you run into any taints or other errors, please let me know.
+The drawbacks of this new hooking method are the playing both frame showing and closing -sounds at the same time when you change from `Chat` to any other tab or vice versa and I had to get rid of the `Always open to Default Tab` -option, because it was getting too hard for me to wrap my head around the logic at the time of rewriting this addon. If you find a good solution to this, I'm more than happy to reimplement the feature. IMHO these are acceptable losses for getting rid of the taints.
