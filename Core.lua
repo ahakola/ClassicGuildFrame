907be36af4b68c0157c450cb5ff199580da37ebf
Unknown <40722602+ahakola@users.noreply.github.com>
2018-08-09 18:14:22 +0300

    Highlight Chat-tab when there are unread messages, go check Config!
    - Updated Russian translations by Hubbotu
    - New feature: Show glow on Chat-tab if there are unread messages in the Chat-frame and any other tab is selected
    - You can disable/change the style of the highlight from Config (Interface -> Addons -> Classic Guild Frame)
    - 8 new lines to translate.

--- Core.lua
+++ Core.lua
@@ -16,14 +16,25 @@ local L = {}
 do -- Translations
 	local LOCALE = GetLocale()
 
+	-- Options
 	L.enableTabs = "Enable Tabs:"
 	L.cWarningText = "You have to leave at least one tab enabled!"
 	L.defaultTab = "Default Tab for the first opening:"
 	L.dWarningText = "You have to select tab that is enabled!"
 	L.miniModeDefault = "Open Chat in Minimized-mode"
 	L.miniModeDefaultHelp = "Pressing the Maximize/Minimize-button on the Chat-frame will change this."
+	L.highlightStyle = "Chat-tab Highlight"
+	L.highlightStyleHelp = "If you have selected highlight style, you get pulsing glow in the Chat-tab to notify you when there are unread messages in the Chat-frame. Select style from the dropdown menu on the left and see the preview on the right."
 	--L.alwaysDefault = "Always open to Default Tab:"
 	--L.alwaysDefaultHelp = "If disabled, addon will open to the last open tab after first opening."
+
+	-- Style names , "None" is same as _G.NONE
+	L.styleBlueHighlight = "Blue Highlight"
+	L.styleYellowHighlight = "Yellow Highlight"
+	L.styleBlueBorder = "Blue Border"
+	L.styleGreenSphere = "Green Sphere"
+	L.styleGoldSphere = "Golden Sphere"
+	L.styleWhiteLine = "White Line"
 
 	if LOCALE == "deDE" then
 
@@ -43,8 +54,10 @@ do -- Translations
 		L["cWarningText"] = "Вы должны оставить хотя бы одну вкладку включенной!"
 		L["defaultTab"] = "Вкладка По умолчанию для первого открытия:"
 		L["dWarningText"] = "Вы должны выбрать вкладку, которая включена!"
-		L["alwaysDefault"] = "Всегда открывайте вкладку по умолчанию:"
-		L["alwaysDefaultHelp"] = "Если отключено, аддон откроется до последней открытой вкладки после первого открытия."
+		L["miniModeDefault"] = "Открыть чат в мини-режиме"
+		L["miniModeDefaultHelp"] = "Нажмите кнопку максимизировать / максимизировать на чате изменит его."
+		--L["alwaysDefault"] = "Всегда открывайте вкладку по умолчанию:"
+		--L["alwaysDefaultHelp"] = "Если отключено, аддон откроется до последней открытой вкладки после первого открытия."
 	elseif LOCALE == "koKR" then
 
 	elseif LOCALE == "zhCN" then
@@ -70,6 +83,17 @@ local tabNames = { -- Names of the tabs
 	GUILD_TAB_INFO
 }
 
+local highlightStyles = { -- Styles for Chat-tab Highlight
+	{ name = NONE, tex = "", l = 0, r = 1, t = 0, b = 1 }, -- None
+	{ name = L.styleBlueHighlight, tex = "Interface\\PaperDollInfoFrame\\UI-Character-Tab-Highlight", l = 0, r = 1, t = 0, b = 1 }, -- Highlight
+	{ name = L.styleYellowHighlight, tex = "Interface\\PaperDollInfoFrame\\UI-Character-Tab-Highlight-yellow", l = 0, r = 1, t = 0, b = 1 }, -- Highlight - Yellow
+	{ name = L.styleBlueBorder, tex = "Interface\\GMChatFrame\\UI-GMStatusFrame-Pulse", l = 0, r = 1, t = 0, b = 1 }, -- GMStatusFrame - Pulse
+	{ name = L.styleGreenSphere, tex = "Interface\\LevelUp\\LevelUpTex", l = 287/512, r = 510/512, t = 364/512, b = 249/512 }, -- LevelUp - Green
+	{ name = L.styleGoldSphere, tex = "Interface\\LevelUp\\LevelUpTex", l = 287/512, r = 510/512, t = 239/512, b = 124/512 }, -- LevelUp - Gold
+	{ name = L.styleWhiteLine, tex = "Interface\\CHATFRAME\\ChatFrameTab-NewMessage", l = 0, r = 1, t = 1, b = 0 }, -- Chat - NewMessage
+}
+
+local defaultStyle = 5
 local defaults = { -- This table defines the addon's default settings:
 	show = {
 		true, -- Chat
@@ -81,7 +105,9 @@ local defaults = { -- This table defines the addon's default settings:
 	},
 	miniMode = false,
 	defaultTab = 1,
-	openAlwaysToDefault = false
+	openAlwaysToDefault = false,
+	highlightStyle = defaultStyle,
+	highlightHold = .5
 }
 
 local cfg
@@ -107,6 +133,37 @@ local function initDB(db, defaults) -- This function copies values from one tabl
 	return db
 end
 
+local function _stopFlashing() -- If flashing, stop flashing
+	local glow = classicTabFrame.Tabs[1].Glow
+	if UIFrameIsFlashing(glow) then
+		UIFrameFlashStop(glow)
+	end
+end
+
+local function _checkUnreadMessages(calledFrom) -- UIFrameFlash the Glow-texture (other option was SetButtonPulse() on the tab)
+	if not cfg.show[1] then return end -- Chat tab is hidden
+	local unreadMessages = CommunitiesUtil.DoesAnyCommunityHaveUnreadMessages()
+	local glow = classicTabFrame.Tabs[1].Glow
+
+	if cfg.highlightStyle > #highlightStyles then
+		cfg.highlightStyle = defaultStyle
+	end
+
+	--print("_checkUnreadMessages", tostring(unreadMessages), calledFrom)
+	if cfg.highlightStyle > 1 and unreadMessages and _G.CommunitiesFrame and not _G.CommunitiesFrame:IsShown() then -- Unread messages
+		local tex = highlightStyles[cfg.highlightStyle].tex
+		local l, r, t, b = highlightStyles[cfg.highlightStyle].l, highlightStyles[cfg.highlightStyle].r, highlightStyles[cfg.highlightStyle].t, highlightStyles[cfg.highlightStyle].b
+		glow:SetTexture(tex)
+		glow:SetTexCoord(l, r, t, b)
+		if not UIFrameIsFlashing(glow) then -- Not flashing, start flashing
+			glow:Show()
+			UIFrameFlash(glow, 1, 1, -1, false, cfg.highlightHold, cfg.highlightHold, ADDON_NAME)
+		end
+	else -- No unread messages
+		_stopFlashing()
+	end
+end
+
 local function _TabShow(self, ...) -- Resize tabs on show
 	PanelTemplates_TabResize(self, 0)
 end
@@ -120,6 +177,8 @@ local function _TabClick(self, ...) -- Handle Tab clicks
 		HideUIPanel(_G.GuildFrame)
 		HideUIPanel(_G.LookingForGuildFrame)
 		ShowUIPanel(_G.CommunitiesFrame)
+
+		_stopFlashing() -- Stop flashing if flashing
 	elseif tabIndex > 1 then -- GuildUI
 		if IsInGuild() then
 			GuildFrame_LoadUI()
@@ -133,6 +192,7 @@ local function _TabClick(self, ...) -- Handle Tab clicks
 			end
 		end
 		HideUIPanel(_G.CommunitiesFrame)
+		_checkUnreadMessages("_TabClick")
 
 		if ( tabIndex == 2 ) then -- News
 			GuildFrame_TabClicked(_G["GuildFrameTab1"])
@@ -150,10 +210,18 @@ end
 
 local function _createClassicTabs() -- Create new tabs for Classic Guild Frame
 	classicTabFrame.Tabs = classicTabFrame.Tabs or {}
+	if #classicTabFrame.Tabs >= 6 then return end -- Buttons already crated
 	for i = 1, 6 do
 		local tab = CreateFrame("Button", "ClassicTab"..i, classicTabFrame, "CharacterFrameTabButtonTemplate", i)
 		if i == 1 then
 			tab:SetPoint("BOTTOMLEFT", 0, -20)
+
+			local t = tab:CreateTexture("$parentHighlightGlow", "ARTWORK")
+			t:SetPoint("TOPLEFT", 12, -2)
+			t:SetPoint("BOTTOMRIGHT", -12, 7)
+			t:SetBlendMode("ADD")
+			t:Hide()
+			tab.Glow = t
 		else
 			tab:SetPoint("LEFT", classicTabFrame.Tabs[i-1], "RIGHT", -15, 0)
 		end
@@ -168,6 +236,9 @@ local function _createClassicTabs() -- Create new tabs for Classic Guild Frame
 end
 
 local function _HandleTabs() -- Handle hiding and anchoring Classic Guild Frame tabs
+	if not classicTabFrame.Tabs then -- No tabs yet, create them
+		_createClassicTabs()
+	end
 	local firstTab = false
 	local previousTab = 0
 	for i, show in ipairs(cfg.show) do
@@ -186,6 +257,8 @@ local function _HandleTabs() -- Handle hiding and anchoring Classic Guild Frame 
 		end
 	end
 
+	_checkUnreadMessages("_HandleTabs")
+
 	if not PanelTemplates_GetSelectedTab(classicTabFrame) then
 		_TabClick(classicTabFrame.Tabs[cfg.defaultTab])
 	end
@@ -201,7 +274,7 @@ local function _hideBlizzardTabs(self) -- Hide Blizzard's own tabs
 			_G["GuildFrameTab"..i]:Hide()
 		end
 	end
-	if not _G["ClassicTab1"] then
+	if not classicTabFrame.Tabs then
 		_createClassicTabs()
 	end
 	classicTabFrame:SetParent(self)
@@ -222,6 +295,8 @@ function f:ADDON_LOADED(event, addon)
 	if addon == ADDON_NAME then
 		ClassicGuildFrameConfig = initDB(ClassicGuildFrameConfig, defaults)
 		cfg = ClassicGuildFrameConfig
+
+		self:RegisterEvent("STREAM_VIEW_MARKER_UPDATED") -- Chat updated
 
 		if IsAddOnLoaded("Blizzard_Communities") and _G.CommunitiesFrame then
 			_G.CommunitiesFrame:HookScript("OnShow", _hideBlizzardTabs)
@@ -250,6 +325,10 @@ function f:ADDON_LOADED(event, addon)
 			hooksecurefunc(_G.CommunitiesFrame.MaximizeMinimizeFrame, "Maximize", _MaximizeHook)
 		end
 	else return end
+end
+
+function f:STREAM_VIEW_MARKER_UPDATED(event, clubId, streamId, lastUnreadTime)
+	_checkUnreadMessages("STREAM_VIEW_MARKER_UPDATED")
 end
 
 local originalShowUIPanel = ShowUIPanel
@@ -299,8 +378,13 @@ do -- Blizzard Options
 
 	Options:Hide()
 	Options:SetScript("OnShow", function(self)
+		if not classicTabFrame.Tabs then -- Create Tabs if they aren't created yet.
+			_createClassicTabs()
+		end
 		local cfg = ClassicGuildFrameConfig
-		local Title, EnableTabsText, CWarningText, DefaultDropDownText, DWarningText, DefaultDropDown, MiniModeDefaultText, MiniModeDefaultCheckBox, MiniModeDefaultHelpText
+		local Title, EnableTabsText, CWarningText, DefaultDropDownText, DWarningText, DefaultDropDown
+		local MiniModeDefaultText, MiniModeDefaultCheckBox, MiniModeDefaultHelpText
+		local HighlightDropDownText, HighlightDropDown, HighlightTestTab, HighlightDropDownHelpText
 		local function CheckBoxOnClick(button)
 			local checked = not not button:GetChecked()
 
@@ -336,7 +420,7 @@ do -- Blizzard Options
 			end
 			_HandleTabs()
 		end
-		local function DropDownOnClick(button)
+		local function DefaultDropDownOnClick(button)
 			if cfg.show[button.value] then
 				cfg.defaultTab = button.value
 				L_UIDropDownMenu_SetSelectedValue(_G[ADDON_NAME.."OptionsDefaultDropDown"], cfg.defaultTab)
@@ -353,7 +437,43 @@ do -- Blizzard Options
 				info.text = tabNames[i]
 				info.value = i
 				info.checked = i == cfg.defaultTab
-				info.func = DropDownOnClick
+				info.func = DefaultDropDownOnClick
+				L_UIDropDownMenu_AddButton(info)
+			end
+		end
+		local function _TestTabUpdate()
+			if cfg.highlightStyle > #highlightStyles then
+				cfg.highlightStyle = defaultStyle
+			end
+
+			local glow = HighlightTestTab.Glow
+			if UIFrameIsFlashing(glow) then
+				UIFrameFlashStop(glow)
+			end
+			if glow and cfg.highlightStyle > 1 then
+				local tex = highlightStyles[cfg.highlightStyle].tex
+				local l, r, t, b = highlightStyles[cfg.highlightStyle].l, highlightStyles[cfg.highlightStyle].r, highlightStyles[cfg.highlightStyle].t, highlightStyles[cfg.highlightStyle].b
+				glow:SetTexture(tex)
+				glow:SetTexCoord(l, r, t, b)
+				if not UIFrameIsFlashing(glow) then
+					glow:Show()
+					UIFrameFlash(glow, 1, 1, -1, false, cfg.highlightHold, cfg.highlightHold, ADDON_NAME)
+				end
+			end
+		end
+		local function HighlightDropDownOnClick(button)
+			cfg.highlightStyle = button.value
+			L_UIDropDownMenu_SetSelectedValue(_G[ADDON_NAME.."OptionsHighlightDropDown"], cfg.highlightStyle)
+			_TestTabUpdate()
+		end
+		local function HighlightDropDown_Initialize()
+			local info = L_UIDropDownMenu_CreateInfo()
+
+			for i, data in ipairs(highlightStyles) do
+				info.text = highlightStyles[i].name
+				info.value = i
+				info.checked = i == cfg.highlightStyle
+				info.func = HighlightDropDownOnClick
 				L_UIDropDownMenu_AddButton(info)
 			end
 		end
@@ -385,12 +505,12 @@ do -- Blizzard Options
 		CWarningText:SetText(L.cWarningText)
 		CWarningText:Hide()
 
-		local DefaultDropDownText = self:CreateFontString("$parentDefaultDropDown", "ARTWORK", "GameFontHighlight")
+		DefaultDropDownText = self:CreateFontString("$parentDefaultDropDownText", "ARTWORK", "GameFontHighlight")
 		DefaultDropDownText:SetPoint("TOPLEFT", CWarningText, "BOTTOMLEFT", -10, -8)
 		DefaultDropDownText:SetJustifyH("LEFT")
 		DefaultDropDownText:SetText(L.defaultTab)
 
-		local DefaultDropDown = CreateFrame("Button", "$parentDefaultDropDown", self, "L_UIDropDownMenuTemplate")
+		DefaultDropDown = CreateFrame("Button", "$parentDefaultDropDown", self, "L_UIDropDownMenuTemplate")
 		L_UIDropDownMenu_Initialize(DefaultDropDown, DefaultDropDown_Initialize)
 		L_UIDropDownMenu_SetSelectedValue(DefaultDropDown, cfg.defaultTab)
 		L_UIDropDownMenu_JustifyText(DefaultDropDown, "CENTER")
@@ -401,7 +521,6 @@ do -- Blizzard Options
 		DWarningText:SetJustifyH("LEFT")
 		DWarningText:SetText(L.dWarningText)
 		DWarningText:Hide()
-
 
 		MiniModeDefaultText = self:CreateFontString("$parentMiniModeDefault", "ARTWORK", "GameFontHighlight")
 		MiniModeDefaultText:SetPoint("TOPLEFT", DWarningText, "BOTTOMLEFT", -10, -8)
@@ -425,8 +544,40 @@ do -- Blizzard Options
 
 		MiniModeDefaultHelpText = self:CreateFontString("$parentMiniModeDefaultHelp", "ARTWORK", "GameFontNormalSmall")
 		MiniModeDefaultHelpText:SetPoint("TOPLEFT", MiniModeDefaultCheckBox, "BOTTOMLEFT", 0, -8)
+		MiniModeDefaultHelpText:SetWidth(571)
 		MiniModeDefaultHelpText:SetJustifyH("LEFT")
 		MiniModeDefaultHelpText:SetText(L.miniModeDefaultHelp)
+
+		HighlightDropDownText = self:CreateFontString("$parentHighlightDropDownText", "ARTWORK", "GameFontHighlight")
+		HighlightDropDownText:SetPoint("TOPLEFT", MiniModeDefaultHelpText, "BOTTOMLEFT", -10, -(8 * 2 + CWarningText:GetHeight())) -- Add extra pixels to make the space appear as big as others
+		HighlightDropDownText:SetJustifyH("LEFT")
+		HighlightDropDownText:SetText(L.highlightStyle)
+
+		HighlightDropDown = CreateFrame("Button", "$parentHighlightDropDown", self, "L_UIDropDownMenuTemplate")
+		L_UIDropDownMenu_Initialize(HighlightDropDown, HighlightDropDown_Initialize)
+		L_UIDropDownMenu_SetSelectedValue(HighlightDropDown, cfg.highlightStyle)
+		L_UIDropDownMenu_JustifyText(HighlightDropDown, "CENTER")
+		HighlightDropDown:SetPoint("TOPLEFT", HighlightDropDownText, "BOTTOMLEFT", 10, -12)
+
+		HighlightTestTab = CreateFrame("Button", "$parentHighlightTestTab", self, "CharacterFrameTabButtonTemplate")
+		HighlightTestTab:SetPoint("TOP", HighlightDropDown)
+		HighlightTestTab:SetText(tabNames[1])
+		PanelTemplates_DeselectTab(HighlightTestTab)
+		PanelTemplates_TabResize(HighlightTestTab, 0)
+		HighlightTestTab:SetPoint("LEFT", floor(self:GetWidth()/2 - HighlightTestTab:GetWidth()/2), 0)
+
+		local t = HighlightTestTab:CreateTexture("$parentHighlightGlow", "ARTWORK")
+		t:SetPoint("TOPLEFT", 12, -2)
+		t:SetPoint("BOTTOMRIGHT", -12, 7)
+		t:SetBlendMode("ADD")
+		HighlightTestTab.Glow = t
+		_TestTabUpdate()
+
+		HighlightDropDownHelpText = self:CreateFontString("$parentHighlightDropDownHelpText", "ARTWORK", "GameFontNormalSmall")
+		HighlightDropDownHelpText:SetPoint("TOPLEFT", HighlightDropDown, "BOTTOMLEFT", 0, -8)
+		HighlightDropDownHelpText:SetWidth(571)
+		HighlightDropDownHelpText:SetJustifyH("LEFT")
+		HighlightDropDownHelpText:SetText(L.highlightStyleHelp)
 
 		self:SetScript("OnShow", nil)
 	end)
